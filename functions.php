0687a33a96ad52da6fcebc91e2b3e5eccb0dc230
Mauro <maurodefalco96@gmail.com>
2023-06-09 14:21:23 +0200

    didn't know if is working, all i do seems shit

--- functions.php
+++ functions.php
@@ -2,14 +2,24 @@
 
 
 function btc_style() {
-	wp_enqueue_style(
-		'fse-style',
-		get_stylesheet_uri(),
-		array(),
-		wp_get_theme()->get( 'Version' )
-	);
+	wp_enqueue_style( 'btc-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ));
 }
 add_action( 'wp_enqueue_scripts', 'btc_style' );
+
+if ( ! function_exists( 'btc_support' ) ) :
+
+	function btc_support() {
+
+		// Add support for block styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Enqueue editor styles.
+		add_editor_style( 'style.css' );
+
+	}
+
+endif;
+add_action( 'after_setup_theme', 'btc_support' );
 
 
 
